4e527f58b69213a581f4632cc4fb7e7a0831d756
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-10 16:19:35 -0400

    Update start_jupyter.sh

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Start a Jupyter Notebook server with no password 
-export MLFLOW_TRACKING_URI="http://127.0.0.1:1235";
-head_path="$(dirname "$(realpath "$0")")/";
-$head_path/../.project-env/bin/jupyter lab --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
+head_path="$(dirname "$(realpath "$0")")/mlflow";
+mkdir -p $head_path;
+backend_path="$head_path/mlruns.db";
+artifacts_path="$head_path/mlartifacts";
+$head_path/../../.project-env/bin/mlflow server --host 127.0.0.1 --port 1235 --backend-store-uri sqlite:///$backend_path --artifacts-destination $artifacts_path
