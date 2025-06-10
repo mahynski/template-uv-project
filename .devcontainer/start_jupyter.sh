b81a365a1ec6a2350117dc6b6e82848358966507
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-10 16:19:27 -0400

    Update start_jupyter.sh

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Start a Jupyter Notebook server with no password so we can easily access it from base env
+# Start a Jupyter Notebook server with no password 
 export MLFLOW_TRACKING_URI="http://127.0.0.1:1235";
-jupyter notebook --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
-# ------------------------------------------------------------------------------------------------------------------- #
-# If notebook is installed in project-env instead of base you can use this instead
-# source /opt/conda/etc/profile.d/conda.sh; # Start a Jupyter Notebook server with no password so we can easily access it
-# conda activate project-env;
-# python -m ipykernel install --user --name=project-env; # Can be re-run multiple times
-# jupyter notebook --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
-# ------------------------------------------------------------------------------------------------------------------- #
+head_path="$(dirname "$(realpath "$0")")/";
+$head_path/../.project-env/bin/jupyter lab --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
