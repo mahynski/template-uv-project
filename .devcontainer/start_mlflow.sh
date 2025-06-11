eca6db3aaab249cacaeda007e07fd28075cffbcc
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-11 10:38:09 -0400

    Update start_mlflow.sh

--- .devcontainer/start_mlflow.sh
+++ .devcontainer/start_mlflow.sh
@@ -3,4 +3,4 @@ head_path="$(dirname "$(realpath "$0")")/mlflow";
 mkdir -p $head_path;
 backend_path="$head_path/mlruns.db";
 artifacts_path="$head_path/mlartifacts";
-mlflow server --host 127.0.0.1 --port 1235 --backend-store-uri sqlite:///$backend_path --artifacts-destination $artifacts_path
+$head_path/../../.project-env/bin/mlflow server --host 127.0.0.1 --port 1235 --backend-store-uri sqlite:///$backend_path --artifacts-destination $artifacts_path
