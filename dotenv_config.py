e4d0d2cc8441598286fe8233f4b40a391b63c899
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-11 11:16:34 -0400

    Create dotenv_config.py

---
+++ dotenv_config.py
@@ -0,0 +1,20 @@
+import pathlib
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class DotEnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=pathlib.Path(__file__).parent.absolute() / ".env")
+    HOST: str = "0.0.0.0"
+    OPENAI_API_KEY: str = ""
+    HF_TOKEN: str = ""
+
+dot_env_settings = DotEnvSettings()
+
+if __name__ == "__main__":
+    print("""
+    To access this from another file:
+    1. Rename .env.example to .env and enter credentials. 
+
+    2. Then you can import and access the information like this:
+    >>> from dotenv_config import dot_env_settings
+    >>> token = dot_env_settings.HF_TOKEN
+    """)
