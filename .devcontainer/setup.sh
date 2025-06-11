86844746d0aa74d3b2367ff39c8887bc3a1200f1
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-11 10:37:10 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,8 @@ for FILENAME in $(ls $head_path/*.requirements); do
     ENV_NAME=$(basename "$FILENAME" .requirements)
     uv venv $head_path/../.$ENV_NAME --python=3.12
     source $head_path/../.$ENV_NAME/bin/activate
+    uv pip install pre-commit mlflow jupyterlab # Always install these
     uv pip install -r $FILENAME
+    pre-commit install
     deactivate
 done
