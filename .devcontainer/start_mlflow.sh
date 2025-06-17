f831f1bfc26281c8cc92fcfbc5c2c50ab87c725b
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-17 08:53:38 -0400

    Update start_mlflow.sh

--- .devcontainer/start_mlflow.sh
+++ .devcontainer/start_mlflow.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Activate your virtual environment, then call this file. MLFlow is installed in each by default.
 head_path="$(dirname "$(realpath "$0")")/mlflow";
 mkdir -p $head_path;
 backend_path="$head_path/mlruns.db";
 artifacts_path="$head_path/mlartifacts";
-$head_path/../../.project-env/bin/mlflow server --host 127.0.0.1 --port 1235 --backend-store-uri sqlite:///$backend_path --artifacts-destination $artifacts_path
+mlflow server --host 127.0.0.1 --port 1235 --backend-store-uri sqlite:///$backend_path --artifacts-destination $artifacts_path
