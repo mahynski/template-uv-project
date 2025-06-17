77ad3b286a9cd163791deb9493847eb9e6cbd52d
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-06-17 08:53:19 -0400

    Update start_jupyter.sh

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Start a Jupyter Notebook server with no password 
+# Activate your virtual environment, then call this file. Jupyter is installed in each by default.
 export MLFLOW_TRACKING_URI="http://127.0.0.1:1235";
-head_path="$(dirname "$(realpath "$0")")/";
-$head_path/../.project-env/bin/jupyter lab --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
+jupyter lab --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
