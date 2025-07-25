70dc8e178631489158ccd49bc6650e399f10e3ce
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-07-25 11:06:57 -0400

    Update start_jupyter.sh

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Activate your virtual environment, then call this file. Jupyter is installed in each by default.
+source "$(dirname "$(realpath "$0")")/../.venv/bin/activate";
 export MLFLOW_TRACKING_URI="http://127.0.0.1:1235";
 jupyter lab --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
