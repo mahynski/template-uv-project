a95108ef53a0e59df918be75d4883b37dfcd5158
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-07-29 10:14:41 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,7 +3,6 @@ devcontainer_path="$(dirname "$(realpath "$0")")/"
 requirements="$devcontainer_path/requirements.txt"
 root="$devcontainer_path/../"
 cd $root
-uv init
-rm main.py
+uv init --bare
 uv add mlflow jupyterlab pydantic-settings
 uv add -r $requirements
