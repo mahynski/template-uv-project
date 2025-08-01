e3932c203077d3fccd2fe4622cb8da73d797550b
Nathan A. Mahynski <23516749+mahynski@users.noreply.github.com>
2025-08-01 09:08:05 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-devcontainer_path="$(dirname "$(realpath "$0")")/"
-requirements="$devcontainer_path/requirements.txt"
-root="$devcontainer_path/../"
-cd $root
-uv init --bare
-uv add mlflow jupyterlab pydantic-settings
-uv add -r $requirements
+devcontainer_path="$(dirname "$(realpath "$0")")/";
+requirements="$devcontainer_path/requirements.txt";
+project_dir="$(realpath $devcontainer_path/../)";
+project_name="$(basename $project_dir)";
+root_dir="$(realpath $project_dir/../)";
+cd $root_dir;
+uv init $project_name --bare --no-workspace;
+cd $project_dir;
+uv add mlflow jupyterlab pydantic-settings deepeval;
+uv add -r $requirements;
